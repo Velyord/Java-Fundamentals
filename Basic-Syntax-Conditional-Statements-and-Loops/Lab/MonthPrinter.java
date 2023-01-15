52bb81c4dd116b1a08c02b63399a1614f1bcef96
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2023-01-15 14:26:19 +0200

    Create MonthPrinter.java

---
+++ Basic-Syntax-Conditional-Statements-and-Loops/Lab/MonthPrinter.java
@@ -0,0 +1,144 @@
+/*
+Условие:
+    Write a program that takes an integer from the console and
+    prints the corresponding month.
+    If the number is more than 12 or less than 1 print "Error!".
+Input:
+    You will receive a single integer on a single line.
+Output:
+    If the number is within the boundaries, print the
+    corresponding month, otherwise, print "Error!".
+Examples:
+    2
+    -> February
+    13
+    -> Error!
+*/
+package SoftUni.Fundamentals.Lab1;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class MonthPrinter {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int month = setValue(smallestInt, biggestInt);
+        
+        switch (month) {
+            case 1:  out.println("January");   break;
+            case 2:  out.println("February");  break;
+            case 3:  out.println("March");     break;
+            case 4:  out.println("April");     break;
+            case 5:  out.println("May");       break;
+            case 6:  out.println("June");      break;
+            case 7:  out.println("July");      break;
+            case 8:  out.println("August");    break;
+            case 9:  out.println("September"); break;
+            case 10: out.println("October");   break;
+            case 11: out.println("November");  break;
+            case 12: out.println("December");  break;
+            default: out.println("Error!");    break;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
