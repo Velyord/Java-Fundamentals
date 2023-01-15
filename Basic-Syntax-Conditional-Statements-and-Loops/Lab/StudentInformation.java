b6cffbf28dfe2af817dfa24f556e6b9c240aa4c8
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2023-01-15 13:52:08 +0200

    Create StudentInformation.java

---
+++ Basic-Syntax-Conditional-Statements-and-Loops/Lab/StudentInformation.java
@@ -0,0 +1,142 @@
+/*
+Условие:
+    You will be given 3 lines of input –
+    student name, age, and average grade.
+    Your task is to print all the info about the student in
+    the following format:
+    "Name: {student name},
+    Age: {student age},
+    Grade: {student grade}".
+Examples:
+    John
+    15
+    -> 5.40
+        Name: John, Age: 15, Grade: 5.40
+    Steve
+    16
+    -> 2.50
+        Name: Steve, Age: 16, Grade: 2.50
+    Marry
+    12
+    -> 6.00
+        Name: Marry, Age: 12, Grade: 6.00
+*/
+package SoftUni.Fundamentals.Lab1;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class StudentInformation {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        String studentName = setValue(null, null);
+        int studentAge = setValue(0, biggestInt);
+        double studentGrade = setValue(2.00, 6.00);
+
+        out.printf(
+                "Name: %s, Age: %d, Grade: %.2f",
+                studentName, studentAge, studentGrade
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
