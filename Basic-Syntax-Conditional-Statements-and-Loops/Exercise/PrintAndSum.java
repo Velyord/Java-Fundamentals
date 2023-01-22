d9356d136f8375e3428ea844bb703fd8591581f5
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2023-01-22 12:51:42 +0200

    Create PrintAndSum.java

---
+++ Basic-Syntax-Conditional-Statements-and-Loops/Exercise/PrintAndSum.java
@@ -0,0 +1,148 @@
+/*
+Условие:
+    Write a program to display numbers from given start to 
+    given end and their sum. All the numbers will be integers. 
+    On the first line, you will receive the start number, 
+    on the second the end number.
+Examples
+    5
+    10	
+    -> 5 6 7 8 9 10
+       Sum: 45
+    0
+    26	
+    -> 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+       22 23 24 25 26 
+       Sum: 351
+    50
+    60	
+    -> 50 51 52 53 54 55 56 57 58 59 60
+       Sum: 605
+*/
+package SoftUni.Fundamentals.Exer1;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class PrintAndSum {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int start = setValue(0, biggestInt);
+        int end = setValue(0, biggestInt);
+        
+        generateNumbersAndSumNumbers(start, end);
+    }
+
+    private static void generateNumbersAndSumNumbers(
+            int start, 
+            int end
+    ) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            out.print(i + " ");
+            sum += i;
+        }
+        out.printf("\nSum: %d", sum);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
