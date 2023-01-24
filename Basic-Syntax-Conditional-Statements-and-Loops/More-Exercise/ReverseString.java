306cbe130b0f5c4dcab74b54d64e79892e2d731a
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2023-01-24 11:09:29 +0200

    Create ReverseString.java

---
+++ Basic-Syntax-Conditional-Statements-and-Loops/More-Exercise/ReverseString.java
@@ -0,0 +1,27 @@
+/*
+Условие:
+    Write a program that reverses a string and prints it on the console.
+Examples:
+    Hello -> olleH
+    SoftUni -> inUtfoS
+    1234 -> 4321
+*/
+package SoftUni.Fundamentals.ME1;
+
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        String word = scanner.nextLine();
+        String reversedWord = "";
+        for (int i=word.length()-1; i>=0; i--)
+            reversedWord += word.charAt(i);
+        out.println(reversedWord);
+    }
+}
