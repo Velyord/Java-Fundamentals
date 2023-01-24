ff1ac598756cc3183f452190140e4cffead28329
Yoan Velyord <yoan.velyord@gmail.com>
2023-01-24 13:20:17 +0200

    Update Messages.java

--- Basic-Syntax-Conditional-Statements-and-Loops/More-Exercise/Messages.java
+++ Basic-Syntax-Conditional-Statements-and-Loops/More-Exercise/Messages.java
@@ -30,7 +30,7 @@ Examples:
     666
     ->
     hello
-    
+
     9
     44
     33
@@ -43,7 +43,7 @@ Examples:
     33
     ->
     hey there
-    
+
     7
     6
     33
@@ -75,7 +75,41 @@ public class Messages {
     static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
-        
+        int messageLength = setValue(0, biggestInt);
+        String message = "";
+        for (int i=0; i<messageLength; i++) {
+            int input = setValue(0, 9999);
+            switch (input) {
+                case 0:    message += " "; break;
+                case 2:    message += "a"; break;
+                case 22:   message += "b"; break;
+                case 222:  message += "c"; break;
+                case 3:    message += "d"; break;
+                case 33:   message += "e"; break;
+                case 333:  message += "f"; break;
+                case 4:    message += "g"; break;
+                case 44:   message += "h"; break;
+                case 444:  message += "i"; break;
+                case 5:    message += "j"; break;
+                case 55:   message += "k"; break;
+                case 555:  message += "l"; break;
+                case 6:    message += "m"; break;
+                case 66:   message += "n"; break;
+                case 666:  message += "o"; break;
+                case 7:    message += "p"; break;
+                case 77:   message += "q"; break;
+                case 777:  message += "r"; break;
+                case 7777: message += "s"; break;
+                case 8:    message += "t"; break;
+                case 88:   message += "u"; break;
+                case 888:  message += "v"; break;
+                case 9:    message += "w"; break;
+                case 99:   message += "x"; break;
+                case 999:  message += "y"; break;
+                case 9999: message += "z"; break;
+            }
+        }
+        out.println(message);
     }
 
     @SuppressWarnings("unchecked")
