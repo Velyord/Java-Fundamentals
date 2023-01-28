6bd45798522f32cc18d90d1fe33216fdbbe7111b
Yoan Velyord <yoan.velyord@gmail.com>
2023-01-28 23:20:26 +0200

    Created ConvertMetersToKilometers

--- DataTypesAndVariables/Lab/ConvertMetersToKilometers.java
+++ DataTypesAndVariables/Lab/ConvertMetersToKilometers.java
@@ -1,66 +1,15 @@
 /*
 Условие:
-    Write a program that helps you buy the games.
-    The valid games are the following games in this table:
-        Name
-        Price
-        OutFall 4
-        $39.99
-        CS: OG
-        $15.99
-        Zplinter Zell
-        $19.99
-        Honored 2
-        $59.99
-        RoverWatch
-        $29.99
-        RoverWatch Origins Edition
-        $39.99
-    On the first line, you will receive your current balance –
-    a floating-point number in the range [0.00…5000.00].
-    Until you receive the command "Game Time", you have to keep buying games.
-    When a game is bought, the user's balance decreases by the price of the game.
-    Additionally, the program should obey the following conditions:
-        • If a game the user is trying to buy is not present in the table above, print "Not Found"
-        and read the next line.
-        • If at any point, the user has $0 left, print "Out of money!" and end the program.
-        • Alternatively, if the user is trying to buy a game that they can't afford,
-        print "Too Expensive" and read the next line.
-    When you receive "Game Time", print the user's remaining money and total spent on games,
-    rounded to the 2nd decimal place.
+    You will be given an integer that will be a distance in meters.
+    Write a program that converts meters to kilometers formatted to the second decimal point.
 Examples:
-    120
-    RoverWatch
-    Honored 2
-    Game Time
-    ->
-    Bought RoverWatch
-    Bought Honored 2
-    Total spent: $89.98. Remaining: $30.02
-
-    19.99
-    Reimen origin
-    RoverWatch
-    Zplinter Zell
-    Game Time
-    ->
-    Not Found
-    Too Expensive
-    Bought Zplinter Zell
-    Out of mo-ney!
-
-    79.99
-    OutFall 4
-    RoverWatch Origins Edition
-    Game Time
-    ->
-    Bought OutFall 4
-    Bought RoverWatch Origins Edition
-    Total spent: $79.98. Remaining: $0.01
+    1852
+    -> 1.85
+    798
+    -> 0.80
 */
-package SoftUni.Fundamentals.ME;
+package DataTypesAndVariables.Lab;
 
-import static java.lang.Math.abs;
 import static java.lang.System.exit;
 import static java.lang.System.out;
 import static java.lang.System.in;
@@ -68,7 +17,7 @@ import static java.lang.System.in;
 import java.util.Scanner;
 import java.util.List;
 
-public class GameStore {
+public class ConvertMetersToKilometers {
     static int smallestInt = Integer.MIN_VALUE;
     static int biggestInt = Integer.MAX_VALUE;
     static double smallestDouble = -1 * Double.MAX_VALUE;
@@ -79,42 +28,13 @@ public class GameStore {
     static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
-        String [] nameOfGames = {"OutFall 4", "CS: OG", "Zplinter Zell", "Honored 2", "RoverWatch", "RoverWatch Origins Edition"};
-        double [] priceOfGames = {39.99, 15.99, 19.99, 59.99, 29.99, 39.99};
-        List<String> nameOfGamesList = List.of(nameOfGames);
+        int meters = setValue(0, biggestInt);
+        double km = convertToKM(meters);
+        out.printf("%.2f", km);
+    }
 
-
-        double originalBalance = setValue(0.00, 5000.00);
-        double balance = originalBalance;
-        String input = scanner.nextLine();
-        boolean hasMoney = true;
-        while(!input.equals("Game Time")) {
-            String nameOfGame = input;
-
-            if (!nameOfGamesList.contains(nameOfGame)){
-                out.println("Not Found");
-            } else {
-                int indexOfGame = nameOfGamesList.indexOf(nameOfGame);
-                double priceOfGame = priceOfGames[indexOfGame];
-                if (balance < priceOfGame)
-                    out.println("Too Expensive");
-                else {
-                    balance -= priceOfGame;
-                    out.printf("Bought %s\n", nameOfGame);
-                }
-            }
-            if (balance == 0) {
-                out.println("Out of money!");
-                hasMoney = false;
-                break;
-            }
-            input = scanner.nextLine();
-        }
-        if (hasMoney) {
-            double totalSpent = abs(balance - originalBalance);
-            double remaining = originalBalance - totalSpent;
-            out.printf("Total spent: $%.2f. Remaining: $%.2f", totalSpent, remaining);
-        }        
+    private static double convertToKM(int meters) {
+        return meters / 1000.0;
     }
 
     @SuppressWarnings("unchecked")
