ae16bcdcefef671ee5b7532167aa86ec599e6405
Yoan Velyord <yoan.velyord@gmail.com>
2023-01-30 21:58:40 +0200

    Created

---
+++ DataTypesAndVariables/Lab/CenturiesToMinutes.java
@@ -0,0 +1,167 @@
+/*
+Условие:
+    Write a program to enter an integer number of centuries and convert it to years, days, hours, and minutes.
+Examples:
+    1
+    ->
+    1 centuries = 100 years = 36524 days = 876581 hours = 52594877 minutes
+
+    5
+    ->
+    5 centuries = 500 years = 182621 days = 4382906 hours = 262974384 minutes
+*/
+package DataTypesAndVariables.Lab;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class CenturiesToMinutes {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static long smallestLong = Long.MIN_VALUE;
+    static long biggestLong = Long.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static short smallestShort = Short.MIN_VALUE;
+    static short biggestShort = Short.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int centuries = setValue(0, biggestInt);
+
+        byte yearsInACentury = 100;
+        int years = centuries * yearsInACentury;
+        double daysInAYear = 365.2422;
+        double days = years * daysInAYear;
+        byte hoursInADay = 24;
+        double hours = days * hoursInADay;
+        byte minutesInAHour = 60;
+        double minutes = hours * minutesInAHour;
+
+        out.printf(
+                "%d centuries = %d years = %.0f days = %.0f hours = %.0f minutes",
+                centuries, years, days, hours, minutes
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|} 0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Long)
+                    value = Long.parseLong(scanner.nextLine());
+                else if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Short)
+                    value = Short.parseShort(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+            if (max instanceof Long) {
+                if ((long) value < (long) min || (long) value > (long) max) {
+                    if ((long) min == 0 && (long) max == biggestLong)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Short) {
+                if ((short) value < (short) min || (short) value > (short) max) {
+                    if ((long) min == 0 && (short) max == biggestShort)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
