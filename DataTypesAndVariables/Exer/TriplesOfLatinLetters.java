7d8fb8bf59652444f48dbb098bb83939b0293347
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-07 16:10:59 +0200

    Created

---
+++ DataTypesAndVariables/Exer/TriplesOfLatinLetters.java
@@ -0,0 +1,189 @@
+/*
+Условие:
+    Write a program to read an integer n and print all triples of the first n small Latin letters,
+    ordered alphabetically:
+Examples:
+    3
+    ->
+    aaa
+    aab
+    aac
+    aba
+    abb
+    abc
+    aca
+    acb
+    acc
+    baa
+    bab
+    bac
+    bba
+    bbb
+    bbc
+    bca
+    bcb
+    bcc
+    caa
+    cab
+    cac
+    cba
+    cbb
+    cbc
+    cca
+    ccb
+    ccc
+
+    2
+    ->
+    aaa
+    aab
+    aba
+    abb
+    baa
+    bab
+    bba
+    bbb
+*/
+package DataTypesAndVariables.Exer;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class TriplesOfLatinLetters {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static long smallestLong = Long.MIN_VALUE;
+    static long biggestLong = Long.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static short smallestShort = Short.MIN_VALUE;
+    static short biggestShort = Short.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int n = setValue(0, biggestInt);
+        for (int i=0; i<n; i++)
+            for (int j=0; j<n; j++)
+                for (int k=0; k<n; k++)
+                    out.printf("%c%c%c%n", (char) (i+97), (char) (j+97), (char) (k+97));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|} 0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Long)
+                    value = Long.parseLong(scanner.nextLine());
+                else if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Short)
+                    value = Short.parseShort(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+            if (max instanceof Long) {
+                if ((long) value < (long) min || (long) value > (long) max) {
+                    if ((long) min == 0 && (long) max == biggestLong)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Short) {
+                if ((short) value < (short) min || (short) value > (short) max) {
+                    if ((long) min == 0 && (short) max == biggestShort)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
