ee77eb4250d734b40abe05fb58fd4c90bc05e19d
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-14 18:00:21 +0200

    Not finished

---
+++ Arrays/Lab/CommonElements.java
@@ -0,0 +1,54 @@
+/*
+Условие:
+    Write a program that prints common elements in two arrays.
+    You have to compare the elements of the second array to the elements of the first.
+Examples:
+    Hey hello 2 4
+    10 hey 4 hello
+    ->
+    4 hello
+
+    S of t un i
+    of i 10 un
+    ->
+    of i un
+
+    i love to code
+    code i love to
+    ->
+    code i love to
+*/
+package Arrays.Lab;
+
+import java.util.Scanner;
+import java.util.List;
+
+import static java.lang.System.*;
+
+public class CommonElements {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        String[] firstArray = scanner.nextLine().split(" ");
+        String[] secondArray = scanner.nextLine().split(" ");
+        Object[] commonArray = {};
+
+        for (String secondArrayItem : secondArray) {
+            for (String firstArrayItem : firstArray) {
+                if (secondArrayItem.equals(firstArrayItem)) {
+                    commonArray = addToArray(secondArrayItem, commonArray);
+                }
+            }
+        }
+        for (Object item : commonArray) {
+            out.println(item + " ");
+        }
+    }
+    @SuppressWarnings("unchecked")
+    private static <T> T[] addToArray(T toBeAdded, T[] array) {
+        T[] newArray = (T[]) new Object[array.length + 1];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[newArray.length - 1] = toBeAdded;
+        return newArray;
+    }
+}
