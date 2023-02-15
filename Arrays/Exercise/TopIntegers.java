f91cd91c7a8ec94badf6b0c5a690affd8ab21c81
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-15 10:41:25 +0200

    Created

---
+++ Arrays/Exercise/TopIntegers.java
@@ -0,0 +1,133 @@
+/*
+Условие:
+    Write a program to find all the top integers in an array.
+    A top integer is an integer that is bigger than all the elements to its right.
+Examples:
+    1 4 3 2
+    ->
+    4 3 2
+
+    14 24 3 19 15 17
+    ->
+    24 19 17
+
+    27 9 42 2 13 45 48
+    ->
+    48
+*/
+package Arrays.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class TopIntegers {
+    static Scanner scanner = new Scanner(in);
+    static int stringCount = 0; // при въвеждане на низ, броячът нараства
+
+    public static void main(String[] args) {
+        int[] numbers = Arrays
+                .stream(setValue().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isBiggestInSection = false;
+            int lastIndex = numbers.length - 1;
+            int topInt = numbers[lastIndex];
+
+            for (int j = i; j < numbers.length - 1; j++) {
+                if (numbers[i] > numbers[j + 1]) {
+                    isBiggestInSection = true;
+                    topInt = numbers[i];
+                } else {
+                    isBiggestInSection = false;
+                }
+            }
+            if (isBiggestInSection || i == lastIndex) {
+                out.print(topInt + " ");
+            }
+        }
+    }
+
+    // метод за откриване на грешни в низ от потребителя
+    private static String setValue() {
+        String value = scanner.nextLine();
+
+        /* ако има забранени символи или не следва задените шаблони,
+        низът на потребителя не се приема и трябва да се въведе нов */
+        if (!hasValidChars(value) || !doesFollowTemplate(value)) {
+            return setValue();
+        } else {
+            return value;
+        }
+    }
+
+    // хващане на специални/забранени символи
+    private static <T> boolean hasValidChars(T value) {
+        // !#$%&'()*+,./:;<=>?@[]^_`{|}
+        // 0123456789
+        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        String specialChars = "!#$%&'()*+,./:;<=>?@[]^_`{|}"; // може да се променят забранените символи
+        boolean isSpecialChar = false;
+        char specialChar = ' ';
+
+        // Търсене на забранен символ чрез сравняване на входните данни с низът със забранени символи
+        for (int i = 0; i < ((String) value).length(); i++) {
+            if (specialChars.contains(Character.toString(value.toString().charAt(i)))) {
+                isSpecialChar = true;
+                specialChar = value.toString().charAt(i); // открит забранен символ
+                break;
+            }
+        }
+
+        // При грешка се показва на потребителя, кой от въведените му символи е забранен
+        if (isSpecialChar) {
+            if (specialChar == ' ') {
+                out.println("Разтоянията не са позволени. Пробвайте пак!");
+            } else {
+                out.printf("%c e неразрешен символ. Пробвайте пак!\n", specialChar);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // подтикване на потребителя да въвежда предварително зададени низове.
+    private static <T> boolean doesFollowTemplate(T value) {
+        stringCount++;
+        String[] requiredStrings = {};
+
+        // тук се нагласят шаблоните на низовете, ако имат такива.
+        if (stringCount == 1) {
+            requiredStrings = new String[]{}; // на първия низ. Празно ако няма такъв.
+        } else if (stringCount == 2) {
+            requiredStrings = new String[]{}; // на втория низ. Празно ако няма такъв.
+        } else { // могат да се добавят и още шаблони преди else. Последния шаблон стои празен.
+            requiredStrings = new String[]{};
+        }
+
+        // ако е зададен шаблон се изпълнява следния код.
+        if (requiredStrings.length != 0) {
+            List<String> requiredList = List.of(requiredStrings); // създава се списък със задължителни входни данни
+
+            if (!requiredList.contains(value.toString())) { // ако се въведе нещо, различно от зададеното в шаблона
+                out.println("Моля въведете един от следните избори:");
+
+                // завърта се цикъл, който да покаже на потребителя, кои са възможните опции
+                out.println(String.join(" | ", requiredStrings)); // разделител
+
+                stringCount--; // не се брои сгрешения низ.
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
