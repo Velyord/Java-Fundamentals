402d6a7b65b76cd2351a31df2a7205126680d8cc
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-21 12:58:32 +0200

    created

--- Arrays/MoreExercise/EncryptSortAndPrintArray.java
+++ Arrays/MoreExercise/EncryptSortAndPrintArray.java
@@ -34,7 +34,7 @@ Examples:
 */
 package Arrays.MoreExercise;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -46,7 +46,37 @@ public class EncryptSortAndPrintArray {
     static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
+        int countOfStrings = setValue(0, Integer.MAX_VALUE);
+        int wordValue = 0;
+        List<Integer> valueList = new ArrayList<>();
 
+        for (int word = 1; word <= countOfStrings; word++) {
+            String input = scanner.nextLine();
+
+            for (int character = 0; character < input.length(); character++) {
+                wordValue += calculateValueFor(character, input);
+            }
+
+            valueList.add(wordValue);
+            wordValue = 0;
+        }
+
+        List<Integer> sortedValueList = valueList.stream().sorted().collect(Collectors.toList());
+        sortedValueList.forEach(System.out::println);
+    }
+
+    private static int calculateValueFor(int character, String input) {
+        int charValue;
+        List<Character> vowelsList = List.of('a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I');
+        char currentChar = input.charAt(character);
+
+        if (vowelsList.contains(currentChar)) {
+            charValue = (int) currentChar * input.length();
+        } else {
+            charValue = (int) currentChar / input.length();
+        }
+
+        return charValue;
     }
 
     // метод за въвеждане на число в дадени граници
@@ -132,102 +162,5 @@ public class EncryptSortAndPrintArray {
         // при грешка трябва да се въведе ново число
         out.printf("Моля въведете число между %s и %s:\n", min, max);
         return false;
-    }
-
-    static int stringCount = 0; // при въвеждане на низ, броячът нараства
-
-    // метод за откриване на грешни в низ от потребителя
-    private static String setValue() {
-        String value = scanner.nextLine();
-
-        /* ако има забранени символи или не следва задените шаблони,
-        низът на потребителя не се приема и трябва да се въведе нов */
-        if (!hasValidChars(value) || !doesFollowTemplate(value)) {
-            return setValue();
-        } else {
-            return value;
-        }
-    }
-
-    // хващане на специални/забранени символи
-    private static <T> boolean hasValidChars(T value) {
-        // !#$%&'()*+,./:;<=>?@[]^_`{|}
-        // 0123456789
-        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-        String specialChars = "!#$%&'()*+,./:;<=>?@[]^_`{|}"; // може да се променят забранените символи
-        boolean isSpecialChar = false;
-        char specialChar = ' ';
-
-        // Търсене на забранен символ чрез сравняване на входните данни с низът със забранени символи
-        for (int i = 0; i < ((String) value).length(); i++) {
-            if (specialChars.contains(Character.toString(value.toString().charAt(i)))) {
-                isSpecialChar = true;
-                specialChar = value.toString().charAt(i); // открит забранен символ
-                break;
-            }
-        }
-
-        // При грешка се показва на потребителя, кой от въведените му символи е забранен
-        if (isSpecialChar) {
-            if (specialChar == ' ') {
-                out.println("Разтоянията не са позволени. Пробвайте пак!");
-            } else {
-                out.printf("%c e неразрешен символ. Пробвайте пак!\n", specialChar);
-            }
-
-            return false;
-        }
-
-        return true;
-    }
-
-    // подтикване на потребителя да въвежда предварително зададени низове.
-    private static <T> boolean doesFollowTemplate(T value) {
-        stringCount++;
-        String[] requiredStrings = {};
-
-        // тук се нагласят шаблоните на низовете, ако имат такива.
-        if (stringCount == 1) {
-            requiredStrings = new String[]{}; // на първия низ. Празно ако няма такъв.
-        } else if (stringCount == 2) {
-            requiredStrings = new String[]{}; // на втория низ. Празно ако няма такъв.
-        } else { // могат да се добавят и още шаблони преди else. Последния шаблон стои празен.
-            requiredStrings = new String[]{};
-        }
-
-        // ако е зададен шаблон се изпълнява следния код.
-        if (requiredStrings.length != 0) {
-            List<String> requiredList = List.of(requiredStrings); // създава се списък със задължителни входни данни
-
-            if (!requiredList.contains(value.toString())) { // ако се въведе нещо, различно от зададеното в шаблона
-                out.println("Моля въведете един от следните избори:");
-
-                // завърта се цикъл, който да покаже на потребителя, кои са възможните опции
-                out.println(String.join(" | ", requiredStrings)); // разделител
-
-                stringCount--; // не се брои сгрешения низ.
-
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    // принтира съдържанието на лист
-    private static <T> void printList(List<T> targets) {
-        System.out.println(
-                targets.stream()
-                        .map(String::valueOf)
-                        .collect(Collectors.joining("|"))
-        );
-    }
-
-    public static <T> void reverseList(List<T> list) {
-        if (list.size() <= 1 || list == null)
-            return;
-        T value = list.remove(0);
-        reverseList(list);
-        list.add(value);
     }
 }
