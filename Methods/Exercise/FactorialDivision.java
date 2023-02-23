227644891525aa8aa705bd3cfca7c944e9541b8a
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-23 18:40:36 +0200

    created /commit 2/

---
+++ Methods/Exercise/FactorialDivision.java
@@ -0,0 +1,126 @@
+/*
+Task:
+    Read two integer numbers. Calculate the factorial of each number.
+    Divide the first result by the second and print the division formatted to the second decimal point.
+Examples:
+    5
+    2
+    ->
+    60.00
+
+    6
+    2
+    ->
+    360.00
+*/
+package Methods.Exercise;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class FactorialDivision {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int firstNumber = setValue(1, Integer.MAX_VALUE);
+        int secondNumber = setValue(1, Integer.MAX_VALUE);
+
+        double result = (double) getFactorial(firstNumber) / getFactorial(secondNumber);
+        out.printf("%.2f", result);
+    }
+
+    private static long getFactorial(int number) {
+        if (number == 1) {
+            return 1;
+        } else {
+            return number * getFactorial(number - 1);
+        }
+    }
+
+    // метод за въвеждане на число в дадени граници
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        String type = getType(max); // намиране на типа му според зададената му максимална граница
+        Object value = setAndCheckInputFor(type); // задаване на числото според неговия тип и проверка за изключения
+
+        // ако не е в дадената граница се повтаря метода
+        if (!isValueBetweenMinAndMax(value, min, max, type)) {
+            return setValue(min, max);
+        } else {
+            return (T) value;
+        }
+    }
+
+    // метод за намиране на типа на число
+    private static <T> String getType(T variable) {
+        if (variable instanceof Double) {
+            return "double";
+        } else if (variable instanceof Float) {
+            return "float";
+        } else if (variable instanceof Long) {
+            return "long";
+        } else if (variable instanceof Integer) {
+            return "int";
+        } else {
+            return "String";
+        }
+    }
+
+    // задава се число според прихванатия му тип, ако се хване изключение, трябва да се въведе ново число
+    @SuppressWarnings("unchecked")
+    private static <T> T setAndCheckInputFor(String type) {
+        Object value;
+
+        try {
+            switch (type) {
+                case "double": value = Double.parseDouble(scanner.nextLine()); break;
+                case "float":  value = Float.parseFloat(scanner.nextLine());   break;
+                case "long":   value = Long.parseLong(scanner.nextLine());     break;
+                case "int":    value = Integer.parseInt(scanner.nextLine());   break;
+                default:       value = null;                                   break;
+            }
+        } catch (Exception e) {
+            out.println("Невалидно число. Пробвайте пак!");
+            return setAndCheckInputFor(type);
+        }
+
+        return (T) value;
+    }
+
+    // метод за проверка на входното число, дали е в зададените граници според неговия тип
+    private static <T> boolean isValueBetweenMinAndMax(T value, T min, T max, String type) {
+        // променливите са за постигане на максимално негативно число при числата с плаваща запетая.
+        double minDouble;
+        float minFloat;
+
+        // проверка дали входното число е правилно избрано според зададените граници
+        switch (type) {
+            case "double":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minDouble = (double) min == Double.MIN_VALUE ? -1 * Double.MAX_VALUE : (double) min;
+                if ((double) value >= minDouble && (double) value <= (double) max) {
+                    return true;
+                } break;
+            case "float":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minFloat = (float) min == Float.MIN_VALUE ? -1 * Float.MAX_VALUE : (float) min;
+                if ((float) value >= minFloat && (float) value <= (float) max) {
+                    return true;
+                } break;
+            case "long":
+                if ((long) value >= (long) min && (long) value <= (long) max) {
+                    return true;
+                } break;
+            case "int":
+                if ((int) value >= (int) min && (int) value <= (int) max) {
+                    return true;
+                } break;
+        }
+
+        // при грешка трябва да се въведе ново число
+        out.printf("Моля въведете число между %s и %s:\n", min, max);
+        return false;
+    }
+}
