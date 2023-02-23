0c125434351d1957620d039eeab23e46cfedec17
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-23 15:29:58 +0200

    created /commit 5/

---
+++ Methods/Lab/GreaterOfTwoValues.java
@@ -0,0 +1,255 @@
+/*
+Условие:
+    You are given two values of the same type as input.
+    The values can be of type int, char or String.
+    Create a method getMax() that returns the greater of the two values:
+Examples:
+    int
+    2
+    16
+    ->
+    16
+
+    char
+    a
+    z
+    ->
+    z
+
+    string
+    Ivan
+    Todor
+    ->
+    Todor
+*/
+package Methods.Lab;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class GreaterOfTwoValues {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        String type = setValue();
+
+        getBiggerOf(type);
+    }
+
+    private static void getBiggerOf(String type) {
+        switch (type) {
+            case "int":
+                int firstInt = setValue(Integer.MIN_VALUE, Integer.MAX_VALUE);
+                int secondInt = setValue(Integer.MIN_VALUE, Integer.MAX_VALUE);
+                int biggerInt = getMax(firstInt, secondInt);
+
+                out.println(biggerInt);
+                break;
+            case "char":
+                char firstChar = setValue().charAt(0);
+                char secondChar = setValue().charAt(0);
+                char biggerChar = getMax(firstChar, secondChar);
+
+                out.println(biggerChar);
+                break;
+            case "string":
+                String firstString = setValue();
+                String secondString = setValue();
+                String biggerString = getMax(firstString, secondString);
+
+                out.println(biggerString);
+                break;
+            default:
+                out.println("Error! Invalid type. Restart the program!");
+                break;
+        }
+    }
+
+    private static String getMax(String firstString, String secondString) {
+        if (firstString.compareTo(secondString) >= 0) {
+            return firstString;
+        } else {
+            return secondString;
+        }
+    }
+
+    private static int getMax(int firstInt, int secondInt) {
+        return Math.max(firstInt, secondInt);
+    }
+
+    private static char getMax(char firstChar, char secondChar) {
+        if (firstChar > secondChar) {
+            return firstChar;
+        } else {
+            return secondChar;
+        }
+    }
+
+    // метод за въвеждане на число в дадени граници
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        String type = getType(max); // намиране на типа му според зададената му максимална граница
+        Object value = setAndCheckInputFor(type); // задаване на числото според неговия тип и проверка за изключения
+
+        // ако не е в дадената граница се повтаря метода
+        if (!isValueBetweenMinAndMax(value, min, max, type)) {
+            return setValue(min, max);
+        } else {
+            return (T) value;
+        }
+    }
+
+    // метод за намиране на типа на число
+    private static <T> String getType(T variable) {
+        if (variable instanceof Double) {
+            return "double";
+        } else if (variable instanceof Float) {
+            return "float";
+        } else if (variable instanceof Long) {
+            return "long";
+        } else if (variable instanceof Integer) {
+            return "int";
+        } else {
+            return "String";
+        }
+    }
+
+    // задава се число според прихванатия му тип, ако се хване изключение, трябва да се въведе ново число
+    @SuppressWarnings("unchecked")
+    private static <T> T setAndCheckInputFor(String type) {
+        Object value;
+
+        try {
+            switch (type) {
+                case "double": value = Double.parseDouble(scanner.nextLine()); break;
+                case "float":  value = Float.parseFloat(scanner.nextLine());   break;
+                case "long":   value = Long.parseLong(scanner.nextLine());     break;
+                case "int":    value = Integer.parseInt(scanner.nextLine());   break;
+                default:       value = null;                                   break;
+            }
+        } catch (Exception e) {
+            out.println("Невалидно число. Пробвайте пак!");
+            return setAndCheckInputFor(type);
+        }
+
+        return (T) value;
+    }
+
+    // метод за проверка на входното число, дали е в зададените граници според неговия тип
+    private static <T> boolean isValueBetweenMinAndMax(T value, T min, T max, String type) {
+        // променливите са за постигане на максимално негативно число при числата с плаваща запетая.
+        double minDouble;
+        float minFloat;
+
+        // проверка дали входното число е правилно избрано според зададените граници
+        switch (type) {
+            case "double":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minDouble = (double) min == Double.MIN_VALUE ? -1 * Double.MAX_VALUE : (double) min;
+                if ((double) value >= minDouble && (double) value <= (double) max) {
+                    return true;
+                } break;
+            case "float":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minFloat = (float) min == Float.MIN_VALUE ? -1 * Float.MAX_VALUE : (float) min;
+                if ((float) value >= minFloat && (float) value <= (float) max) {
+                    return true;
+                } break;
+            case "long":
+                if ((long) value >= (long) min && (long) value <= (long) max) {
+                    return true;
+                } break;
+            case "int":
+                if ((int) value >= (int) min && (int) value <= (int) max) {
+                    return true;
+                } break;
+        }
+
+        // при грешка трябва да се въведе ново число
+        out.printf("Моля въведете число между %s и %s:\n", min, max);
+        return false;
+    }
+
+    static int stringCount = 0; // при въвеждане на низ, броячът нараства
+
+    // метод за откриване на грешни в низ от потребителя
+    private static String setValue() {
+        String value = scanner.nextLine();
+
+        /* ако има забранени символи или не следва задените шаблони,
+        низът на потребителя не се приема и трябва да се въведе нов */
+        if (!hasValidChars(value) || !doesFollowTemplate(value)) {
+            return setValue();
+        } else {
+            return value;
+        }
+    }
+
+    // хващане на специални/забранени символи
+    private static <T> boolean hasValidChars(T value) {
+        // !#$%&'()*+,./:;<=>?@[]^_`{|}
+        // 0123456789
+        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        String specialChars = ""; // може да се променят забранените символи
+        boolean isSpecialChar = false;
+        char specialChar = ' ';
+
+        // Търсене на забранен символ чрез сравняване на входните данни с низът със забранени символи
+        for (int i = 0; i < ((String) value).length(); i++) {
+            if (specialChars.contains(Character.toString(value.toString().charAt(i)))) {
+                isSpecialChar = true;
+                specialChar = value.toString().charAt(i); // открит забранен символ
+                break;
+            }
+        }
+
+        // При грешка се показва на потребителя, кой от въведените му символи е забранен
+        if (isSpecialChar) {
+            if (specialChar == ' ') {
+                out.println("Разтоянията не са позволени. Пробвайте пак!");
+            } else {
+                out.printf("%c e неразрешен символ. Пробвайте пак!\n", specialChar);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // подтикване на потребителя да въвежда предварително зададени низове.
+    private static <T> boolean doesFollowTemplate(T value) {
+        stringCount++;
+        String[] requiredStrings = {};
+
+        // тук се нагласят шаблоните на низовете, ако имат такива.
+        if (stringCount == 1) {
+            requiredStrings = new String[]{"int", "char", "string"}; // на първия низ. Празно ако няма такъв.
+        } else if (stringCount == 2) {
+            requiredStrings = new String[]{}; // на втория низ. Празно ако няма такъв.
+        } else { // могат да се добавят и още шаблони преди else. Последния шаблон стои празен.
+            requiredStrings = new String[]{};
+        }
+
+        // ако е зададен шаблон се изпълнява следния код.
+        if (requiredStrings.length != 0) {
+            List<String> requiredList = List.of(requiredStrings); // създава се списък със задължителни входни данни
+
+            if (!requiredList.contains(value.toString())) { // ако се въведе нещо, различно от зададеното в шаблона
+                out.println("Моля въведете един от следните избори:");
+
+                // завърта се цикъл, който да покаже на потребителя, кои са възможните опции
+                out.println(String.join(" | ", requiredStrings)); // разделител
+
+                stringCount--; // не се брои сгрешения низ.
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
