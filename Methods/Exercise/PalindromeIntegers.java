01adfd21505f923be9bcec921e67a3036678bfc5
Yoan Velyord <yoan.velyord@gmail.com>
2023-02-24 10:41:51 +0200

    created /c3/

---
+++ Methods/Exercise/PalindromeIntegers.java
@@ -0,0 +1,151 @@
+/*
+Task:
+    A palindrome is a number that reads the same backward as forward, such as 323 or 1001.
+    Write a program that reads a positive integer number until you receive "END".
+    For each numbered print, whether the number is palindrome or not.
+Examples:
+    123
+    323
+    421
+    121
+    END
+    ->
+    false
+    true
+    false
+    true
+
+    32
+    2
+    232
+    1010
+    END
+    ->
+    false
+    true
+    true
+    false
+*/
+package Methods.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class PalindromeIntegers {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        enterNumbers();
+    }
+
+    private static void enterNumbers() {
+        String input = setValue();
+
+        while (!input.equals("END")) {
+            out.println(isPalindrome(input));
+
+            input = setValue();
+        }
+    }
+
+    private static boolean isPalindrome(String input) {
+        List<Integer> digitList = Arrays
+                .stream(input.split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        for (int i = 0; i <= digitList.size() / 2; i++) {
+            if (digitList.get(i) != digitList.get(digitList.size() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static int stringCount = 0; // при въвеждане на низ, броячът нараства
+
+    // метод за откриване на грешни в низ от потребителя
+    private static String setValue() {
+        String value = scanner.nextLine();
+
+        /* ако има забранени символи или не следва задените шаблони,
+        низът на потребителя не се приема и трябва да се въведе нов */
+        if (!hasValidChars(value) || !doesFollowTemplate(value)) {
+            return setValue();
+        } else {
+            return value;
+        }
+    }
+
+    // хващане на специални/забранени символи
+    private static <T> boolean hasValidChars(T value) {
+        // !#$%&'()*+,./:;<=>?@[]^_`{|}
+        // 0123456789
+        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        String specialChars = "!#$%&'()*+,./:;<=>?@[]^_`{|}- abcdefghijklmnopqrstuvwxyzABCFGHIJKLMOPQRSTUVWXYZ"; // може да се променят забранените символи
+        boolean isSpecialChar = false;
+        char specialChar = ' ';
+
+        // Търсене на забранен символ чрез сравняване на входните данни с низът със забранени символи
+        for (int i = 0; i < ((String) value).length(); i++) {
+            if (specialChars.contains(Character.toString(value.toString().charAt(i)))) {
+                isSpecialChar = true;
+                specialChar = value.toString().charAt(i); // открит забранен символ
+                break;
+            }
+        }
+
+        // При грешка се показва на потребителя, кой от въведените му символи е забранен
+        if (isSpecialChar) {
+            if (specialChar == ' ') {
+                out.println("Разтоянията не са позволени. Пробвайте пак!");
+            } else {
+                out.printf("%c e неразрешен символ. Пробвайте пак!\n", specialChar);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // подтикване на потребителя да въвежда предварително зададени низове.
+    private static <T> boolean doesFollowTemplate(T value) {
+        stringCount++;
+        String[] requiredStrings = {};
+
+        // тук се нагласят шаблоните на низовете, ако имат такива.
+        if (stringCount == 1) {
+            requiredStrings = new String[]{}; // на първия низ. Празно ако няма такъв.
+        } else if (stringCount == 2) {
+            requiredStrings = new String[]{}; // на втория низ. Празно ако няма такъв.
+        } else { // могат да се добавят и още шаблони преди else. Последния шаблон стои празен.
+            requiredStrings = new String[]{};
+        }
+
+        // ако е зададен шаблон се изпълнява следния код.
+        if (requiredStrings.length != 0) {
+            List<String> requiredList = List.of(requiredStrings); // създава се списък със задължителни входни данни
+
+            if (!requiredList.contains(value.toString())) { // ако се въведе нещо, различно от зададеното в шаблона
+                out.println("Моля въведете един от следните избори:");
+
+                // завърта се цикъл, който да покаже на потребителя, кои са възможните опции
+                out.println(String.join(" | ", requiredStrings)); // разделител
+
+                stringCount--; // не се брои сгрешения низ.
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
