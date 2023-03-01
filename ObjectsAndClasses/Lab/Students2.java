c4a148689d38460b60421714d6e9840d48f76722
Yoan Velyord <yoan.velyord@gmail.com>
2023-03-01 11:48:31 +0200

    task edited /c4/

--- ObjectsAndClasses/Lab/Students2.java
+++ ObjectsAndClasses/Lab/Students2.java
@@ -1,31 +1,30 @@
 /*
 Task:
-    Define a class Student, which holds the following information about students:
-    first name, last name, age, and hometown.
-    Read the list of students until you receive the "end" command.
-    After that, you will receive a city name.
-    Print only students which are from the given city, in the following format:
-    "{firstName} {lastName} is {age} years old".
+    Use the class from the previous problem.
+    If you receive a student who already exists (first name and last name should be unique),
+    overwrite the information.
 Examples:
     John Smith 15 Sofia
-    Peter Ivanov 14 Plovdiv
-    Linda Bridge 16 Sofia
+    John Smith 16 Sofia
+    Linda Bridge 17 Sofia
     Simon Stone 12 Varna
     end
     Sofia
     ->
-    John Smith is 15 years old
-    Linda Bridge is 16 years old
-    Anthony Taylor 15 Chicago
-    David Anderson 16 Washington
-    Jack Lewis 14 Chicago
-    David Lee 14 Chicago
+    John Smith is 16 years old
+    Linda Bridge is 17 years old
+
+    J S 3 S
+    Peter Ivanov 14 P
+    P J 104 S
+    J P 61 S
+    Simon Stone 12 Varna
+    Simon Sone 12 Varna
     end
-    Chicago
+    Varna
     ->
-    Anthony Taylor is 15 years old
-    Jack Lewis is 14 years old
-    David Lee is 14 years old
+    Simon Stone is 12 years old
+    Simon Sone is 12 years old
 */
 package ObjectsAndClasses.Lab;
 
