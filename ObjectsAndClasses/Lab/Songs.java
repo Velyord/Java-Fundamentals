8c20183c20cdaa13b58150fd8517b0a83804ad3a
Yoan Velyord <yoan.velyord@gmail.com>
2023-03-01 10:21:52 +0200

    created /c2/

---
+++ ObjectsAndClasses/Lab/Songs.java
@@ -0,0 +1,277 @@
+/*
+Task:
+    Define a class Song, which holds the following information about songs: Type List, Name, and Time.
+    On the first line, you will receive the number of songs - N.
+    On the next N-lines, you will be receiving data in the following format: "{typeList}_{name}_{time}".
+    On the last line, you will receive "Type List" / "all".
+    Print only the names of the songs which are from that Type List / All songs.
+Examples:
+    3
+    favourite_DownTown_3:14
+    favourite_Kiss_4:16
+    favourite_Smooth Criminal_4:01
+    favourite
+    ->
+    DownTown
+    Kiss
+    Smooth Criminal
+
+    4
+    favourite_DownTown_3:14
+    listenLater_Andalouse_3:24
+    favourite_In To The Night_3:58
+    favourite_Live It Up_3:48
+    listenLater
+    ->
+    Andalouse
+
+    2
+    like_Replay_3:15
+    ban_Photoshop_3:48
+    all
+    ->
+    Replay
+    Photoshop
+*/
+package ObjectsAndClasses.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Songs {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        class Song {
+            private final String name;
+            private final String typeList;
+
+            public Song (String typeList, String name, String time) {
+                this.typeList = typeList;
+                this.name = name;
+            }
+
+            public String getType() {
+                return this.typeList;
+            }
+
+            public String getName() {
+                return this.name;
+            }
+        }
+
+        int numberOfSongs = setValue(0, Integer.MAX_VALUE);
+        List<String> typeList = new ArrayList<>();
+        List<String> all = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfSongs; i++) {
+            String[] input = setValue().split("_");
+            String songType = input[0];
+            String songName = input[1];
+            String songTime = input[2];
+            Song song = new Song(songType, songName, songTime);
+
+            all.add(song.getName());
+            typeList.add(song.getType() + "_" + song.getName());
+        }
+
+        String showType = setValue();
+
+        if (showType.equals("all")) {
+            all.forEach(out::println);
+        } else {
+            for (String item : typeList) {
+                if (item.startsWith(showType)) {
+                    out.println(item.substring(item.indexOf("_") + 1));
+                }
+            }
+        }
+    }
+
+    // метод за въвеждане на число в дадени граници
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        String type = getType(max); // намиране на типа му според зададената му максимална граница
+        Object value = setAndCheckInputFor(type); // задаване на числото според неговия тип и проверка за изключения
+
+        // ако не е в дадената граница се повтаря метода
+        if (!isValueBetweenMinAndMax(value, min, max, type)) {
+            return setValue(min, max);
+        } else {
+            return (T) value;
+        }
+    }
+
+    // метод за намиране на типа на число
+    private static <T> String getType(T variable) {
+        if (variable instanceof Double) {
+            return "double";
+        } else if (variable instanceof Float) {
+            return "float";
+        } else if (variable instanceof Long) {
+            return "long";
+        } else if (variable instanceof Integer) {
+            return "int";
+        } else {
+            return "String";
+        }
+    }
+
+    // задава се число според прихванатия му тип, ако се хване изключение, трябва да се въведе ново число
+    @SuppressWarnings("unchecked")
+    private static <T> T setAndCheckInputFor(String type) {
+        Object value;
+
+        try {
+            switch (type) {
+                case "double": value = Double.parseDouble(scanner.nextLine()); break;
+                case "float":  value = Float.parseFloat(scanner.nextLine());   break;
+                case "long":   value = Long.parseLong(scanner.nextLine());     break;
+                case "int":    value = Integer.parseInt(scanner.nextLine());   break;
+                default:       value = null;                                   break;
+            }
+        } catch (Exception e) {
+            out.println("Невалидно число. Пробвайте пак!");
+            return setAndCheckInputFor(type);
+        }
+
+        return (T) value;
+    }
+
+    // метод за проверка на входното число, дали е в зададените граници според неговия тип
+    private static <T> boolean isValueBetweenMinAndMax(T value, T min, T max, String type) {
+        // променливите са за постигане на максимално негативно число при числата с плаваща запетая.
+        double minDouble;
+        float minFloat;
+
+        // проверка дали входното число е правилно избрано според зададените граници
+        switch (type) {
+            case "double":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minDouble = (double) min == Double.MIN_VALUE ? -1 * Double.MAX_VALUE : (double) min;
+                if ((double) value >= minDouble && (double) value <= (double) max) {
+                    return true;
+                } break;
+            case "float":
+                // ако е зададено най-малкото число, го замести с най-голямото умножено по -1
+                minFloat = (float) min == Float.MIN_VALUE ? -1 * Float.MAX_VALUE : (float) min;
+                if ((float) value >= minFloat && (float) value <= (float) max) {
+                    return true;
+                } break;
+            case "long":
+                if ((long) value >= (long) min && (long) value <= (long) max) {
+                    return true;
+                } break;
+            case "int":
+                if ((int) value >= (int) min && (int) value <= (int) max) {
+                    return true;
+                } break;
+        }
+
+        // при грешка трябва да се въведе ново число
+        out.printf("Моля въведете число между %s и %s:\n", min, max);
+        return false;
+    }
+
+    static int stringCount = 0; // при въвеждане на низ, броячът нараства
+
+    // метод за откриване на грешни в низ от потребителя
+    private static String setValue() {
+        String value = scanner.nextLine();
+
+        /* ако има забранени символи или не следва задените шаблони,
+        низът на потребителя не се приема и трябва да се въведе нов */
+        if (!hasValidChars(value) || !doesFollowTemplate(value)) {
+            return setValue();
+        } else {
+            return value;
+        }
+    }
+
+    // хващане на специални/забранени символи
+    private static <T> boolean hasValidChars(T value) {
+        // !#$%&'()*+,./:;<=>?@[]^_`{|}
+        // 0123456789
+        // abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        String specialChars = "!#$%&'()*+,./;<=>?@[]^`{|}"; // може да се променят забранените символи
+        boolean isSpecialChar = false;
+        char specialChar = ' ';
+
+        // Търсене на забранен символ чрез сравняване на входните данни с низът със забранени символи
+        for (int i = 0; i < ((String) value).length(); i++) {
+            if (specialChars.contains(Character.toString(value.toString().charAt(i)))) {
+                isSpecialChar = true;
+                specialChar = value.toString().charAt(i); // открит забранен символ
+                break;
+            }
+        }
+
+        // При грешка се показва на потребителя, кой от въведените му символи е забранен
+        if (isSpecialChar) {
+            if (specialChar == ' ') {
+                out.println("Разтоянията не са позволени. Пробвайте пак!");
+            } else {
+                out.printf("%c e неразрешен символ. Пробвайте пак!\n", specialChar);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // подтикване на потребителя да въвежда предварително зададени низове.
+    private static <T> boolean doesFollowTemplate(T value) {
+        stringCount++;
+        String[] requiredStrings = {};
+
+        // тук се нагласят шаблоните на низовете, ако имат такива.
+        if (stringCount == 1) {
+            requiredStrings = new String[]{}; // на първия низ. Празно ако няма такъв.
+        } else if (stringCount == 2) {
+            requiredStrings = new String[]{}; // на втория низ. Празно ако няма такъв.
+        } else { // могат да се добавят и още шаблони преди else. Последния шаблон стои празен.
+            requiredStrings = new String[]{};
+        }
+
+        // ако е зададен шаблон се изпълнява следния код.
+        if (requiredStrings.length != 0) {
+            List<String> requiredList = List.of(requiredStrings); // създава се списък със задължителни входни данни
+
+            if (!requiredList.contains(value.toString())) { // ако се въведе нещо, различно от зададеното в шаблона
+                out.println("Моля въведете един от следните избори:");
+
+                // завърта се цикъл, който да покаже на потребителя, кои са възможните опции
+                out.println(String.join(" | ", requiredStrings)); // разделител
+
+                stringCount--; // не се брои сгрешения низ.
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // принтира съдържанието на лист
+    private static <T> void printList(List<T> targets) {
+        System.out.println(
+                targets.stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining("|"))
+        );
+    }
+
+    public static <T> void reverseList(List<T> list) {
+        if (list.size() <= 1 || list == null)
+            return;
+        T value = list.remove(0);
+        reverseList(list);
+        list.add(value);
+    }
+}
