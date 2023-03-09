7ed9dabb9592e6f47d1ca05735705492a322f4d5
Yoan Velyord <yoan.velyord@gmail.com>
2023-03-09 14:56:59 +0200

    Main Template /c2/

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+Task:
+
+ */
+//package PackageName;
+
+import static ObjectsAndClasses.Exercise.Articles.NumberValidator.setValue;
+import static ObjectsAndClasses.Exercise.Articles.StringValidator.setValue;
+import static ObjectsAndClasses.Exercise.Articles.PersonalUtils.*;
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
