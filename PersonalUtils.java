ec6cf5d1003dd97b654d4800bbc834779e97961f
Yoan Velyord <yoan.velyord@gmail.com>
2023-03-09 14:44:55 +0200

    new Templates

---
+++ PersonalUtils.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+import static java.lang.System.out;
+
+public class PersonalUtils {
+    public static <T> void printList(List<T> numberList, String delimiter) {
+        out.println(String.join(delimiter, numberList.toString()));
+    }
+}
