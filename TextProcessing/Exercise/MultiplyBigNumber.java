0359ea5ca3011f3884d58038c91b5ac3e83b0ef1
Yoan Velyord <yoan.velyord@gmail.com>
2023-03-16 17:18:17 +0200

    added task description /c5/

--- TextProcessing/Exercise/MultiplyBigNumber.java
+++ TextProcessing/Exercise/MultiplyBigNumber.java
@@ -1,3 +1,25 @@
+/*
+Task:
+    You are given two lines – the first one can be a really big number (0 to 1050).
+    The second one will be a single-digit number (0 to 9).
+    You must display the product of these numbers.
+    Note: do not use the BigInteger class.
+Examples:
+    23
+    2
+    ->
+    46
+
+    9999
+    9
+    ->
+    89991
+
+    923847238931983192462832102
+    4
+    ->
+    3695388955727932769851328408
+ */
 package TextProcessing.Exercise;
 
 import java.util.Scanner;
