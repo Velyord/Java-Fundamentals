5737c356fc783a9da28608589c2f45ac66998e2a
Yoan Velyord <yoan.velyord@gmail.com>
2023-04-05 10:58:49 +0300

    description added /c3/

--- FundamentalsExams/MidExam05/Inventory.java
+++ FundamentalsExams/MidExam05/Inventory.java
@@ -1,3 +1,34 @@
+/*
+Task:
+    As a young traveler, you gather items and craft new items.
+Input / Constraints:
+    You will receive a journal with some collecting items, separated with a comma and a space (", ").
+    After that, until receiving "Craft!" you will be receiving different commands split by " - ":
+    • "Collect - {item}" - you should add the given item to your inventory.
+    If the item already exists, you should skip this line.
+    • "Drop - {item}" - you should remove the item from your inventory if it exists.
+    • "Combine Items - {old_item}:{new_item}" - you should check if the old item exists.
+    If so, add the new item after the old one. Otherwise, ignore the command.
+    • "Renew – {item}" – if the given item exists,
+    you should change its position and put it last in your inventory.
+Output:
+    After receiving "Craft!" print the items in your inventory, separated by ", ".
+Examples:
+    Iron, Wood, Sword
+    Collect - Gold
+    Drop - Wood
+    Craft!
+    ->
+    Iron, Sword, Gold
+
+    Iron, Sword
+    Drop - Bronze
+    Combine Items - Sword:Bow
+    Renew - Iron
+    Craft!
+    ->
+    Sword, Bow, Iron
+ */
 package FundamentalsExams.MidExam05;
 
 import java.util.Arrays;
