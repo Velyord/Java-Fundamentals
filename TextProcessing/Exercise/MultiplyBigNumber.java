acd8c7180f3686120698034edb876baa6835c5ea
Yoan Velyord <yoan.velyord@gmail.com>
2023-04-13 13:09:36 +0300

    !solved /c?/

--- TextProcessing/Exercise/MultiplyBigNumber.java
+++ TextProcessing/Exercise/MultiplyBigNumber.java
@@ -1,6 +1,6 @@
 /*
 Task:
-    You are given two lines – the first one can be a really big number (0 to 1050).
+    You are given two lines – the first one can be a really big number (0 to 10^50).
     The second one will be a single-digit number (0 to 9).
     You must display the product of these numbers.
     Note: do not use the BigInteger class.
